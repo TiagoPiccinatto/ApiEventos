269b4c880a30a3f8b8d056f1b7a8b3c463477ffc
TiagoPiccinatto <tiagoaff2030@gmail.com>
2022-05-27 22:26:43 -0300

    Criando Filtro De Pesquisa

--- Front/EventoAngular-App/src/app/eventos/eventos.component.ts
+++ Front/EventoAngular-App/src/app/eventos/eventos.component.ts
@@ -8,12 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: {tema:string, local:string, id:number}) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+         evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1 || evento.id.toString().indexOf(filtrarPor) !== -1
+    )
+  }
   ngIf="exibirimagem";
   exibirimagem = false;
   public eventos: any = [];
+  public eventosfiltrados: any = [];
 
   tamanhoimg: number = 150;
   tamanhomargin = 2;
+  private _filtrolista: string = "";
+
+  public get filtrolista(){
+    return this._filtrolista;
+  }
+
+
+  public set filtrolista(value: string){
+    this._filtrolista = value;
+    this.eventosfiltrados = this.filtrolista ? this.filtrarEventos(this.filtrolista) : this.eventos;
+  }
+
 
 
   constructor(private http: HttpClient) { }
@@ -24,7 +44,10 @@ export class EventosComponent implements OnInit {
 
   public getEvento(): void {
     this.http.get('https://localhost:5001/api/test/Evento').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response,
+        this.eventosfiltrados = this.eventos;
+      }
 
     );
 
