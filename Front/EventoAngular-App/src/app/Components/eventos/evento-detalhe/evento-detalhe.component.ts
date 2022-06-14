7e95ad0f47e5e872911c23238d92704f70b7e305
TiagoPiccinatto <tiagoaff2030@gmail.com>
2022-06-14 17:53:39 -0300

    Reactive Form

--- Front/EventoAngular-App/src/app/Components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Front/EventoAngular-App/src/app/Components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-evento-detalhe',
@@ -10,22 +10,26 @@ export class EventoDetalheComponent implements OnInit {
 
 
   form!: FormGroup;
-  constructor() { }
+
+  constructor(
+    private fb: FormBuilder,) { }
 
   ngOnInit(): void {
+  this.validacao();
   }
 
 
   public validacao(): void {
   this.form = new FormGroup({
-  local: new FormControl(),
-  dataEvento: new FormControl(),
-  tema: new FormControl(),
-  qtPessoas: new FormControl(),
-  lote: new FormControl(),
-  imagemUrl: new FormControl(),
-  telefone: new FormControl(),
-  email: new FormControl(),
+  local: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(50)]),
+  dataEvento: new FormControl('', Validators.required),
+  tema: new FormControl('', [Validators.required, Validators.maxLength(50), Validators.minLength(4)]),
+  qtPessoas: new FormControl('', [Validators.required, Validators.max(300)]),
+  lote: new FormControl('', Validators.required),
+  imagemUrl: new FormControl('', Validators.required),
+  telefone: new FormControl('', Validators.required),
+  email: new FormControl('', [Validators.required, Validators.email]),
   });
   }
+
 }
